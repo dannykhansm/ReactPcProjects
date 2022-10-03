8e472282cf81ee3b0a9d0cda2e7a17a2b1cb7a3b
Danish Khan <danishkhansm21@gmail.com>
2022-10-03 18:08:33 +0530

    cleaing a code with array destructuring

--- counter/src/components/Counters.jsx
+++ counter/src/components/Counters.jsx
@@ -2,20 +2,18 @@ import React, { Component } from "react";
 import Counter from "./Counter";
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncriment } = this.props;
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
             counter={counter}
-            onIncriment={this.props.onIncriment}
-            onDelete={this.props.onDelete}
+            onIncriment={onIncriment}
+            onDelete={onDelete}
           />
         ))}
       </div>
--- counter/src/components/navBar.jsx
+++ counter/src/components/navBar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <div>
       <nav className="navbar bg-light">
         <span className="navbar-brand mb-0 h1">
           Navbar
           <span className="badge bg-secondary rounded-pill m-2">
-            {props.totalCounters}
+            {totalCounters}
           </span>
         </span>
       </nav>
