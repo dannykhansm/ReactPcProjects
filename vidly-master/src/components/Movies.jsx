4c197258927949a8e872f77f1f2fde58a15048ba
Danish Khan <danishkhansm21@gmail.com>
2022-10-04 11:15:42 +0530

    added filter with Genres and seprate moviesTable

--- vidly-master/src/components/Movies.jsx
+++ vidly-master/src/components/Movies.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
-import Like from "./common/Like";
 import Pagination from "./common/Pagination";
 import { paginate } from "../utils/paginate";
 import ListGroup from "./common/listGroup";
+import MoviesTable from "./MoviesTable";
 class Movies extends Component {
   state = {
     movies: [],
@@ -13,7 +13,8 @@ class Movies extends Component {
     pageSize: 4,
   };
   componentDidMount() {
-    this.setState({ movies: getMovies(), geners: getGenres() });
+    const geners = [{ name: "All Genres" }, ...getGenres()];
+    this.setState({ movies: getMovies(), geners });
   }
   handleDelete = (movie) => {
     const filterdMovies = this.state.movies.filter(
@@ -31,66 +32,43 @@ class Movies extends Component {
   hanndlePageChange = (page) => {
     this.setState({ currentPage: page });
   };
-  handleGenresSelect = (gener) => {
-    console.log(gener);
+  handleGenresSelect = (genre) => {
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allMovies,
+    } = this.state;
     if (count === 0) return <p>There are no Movies in the database</p>;
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
         <div className="col-sm-3">
           <ListGroup
             items={this.state.geners}
-            // textProperty="name"
-            // we hava assign same value as defaultProps
-            // valueProperty="_id"
+            // we hava assign same value as defaultProps textProperty="name" valueProperty="_id"
             onItemSelect={this.handleGenresSelect}
+            selectedItem={this.state.selectedGenre}
           />
         </div>
         <div className="col">
-          <p>Showing {count} Movies in Database</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      liked={movie.liked}
-                      onClick={() => this.handleLike(movie)}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-danger btn-sm"
-                      onClick={() => this.handleDelete(movie)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <p>Showing {filtered.length} Movies in Database</p>
+          <MoviesTable
+            movies={movies}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+          />
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.hanndlePageChange}
