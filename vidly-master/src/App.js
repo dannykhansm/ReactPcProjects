94d4391efda65b78e0e2f7e57502797d25598982
Danish Khan <danishkhansm21@gmail.com>
2022-10-10 11:06:44 +0530

    added routing with v4

--- vidly-master/src/App.js
+++ vidly-master/src/App.js
@@ -1,12 +1,29 @@
-import "./App.css"
-import Movies from "./components/Movies"
+import { Redirect, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/Movies";
+import Customer from "./components/Customer";
+import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
+import Rentals from "./components/Rentals";
+import MovieForm from "./MovieForm";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
-  )
+    <div>
+      <Navbar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customer} />
+          <Route path="/Rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vidly-master/src/components/MoviesTable.jsx
+++ vidly-master/src/components/MoviesTable.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import Like from "./common/Like";
 import Table from "./common/Table";
+import { Link } from "react-router-dom";
 
 class MoviesTable extends Component {
   column = [
-    { path: "title", Title: "Title" },
+    {
+      path: "title",
+      Title: "Title",
+      content: (movie) => <Link to={`movies/${movie._id}`}>{movie.title}</Link>,
+    },
     { path: "genre.name", Title: "Genre" },
     { path: "numberInStock", Title: "Stock" },
     { path: "dailyRentalRate", Title: "Rate" },
